bf829d864ead158ada529bc541662c80e79bef68
sachajoy <me.arihant.banthia@gmail.com>
2022-01-25 11:13:34 +0530

    Project Initilization

---
+++ parctice/src/main/java/com/billsystem/parctice/ParcticeApplication.java
@@ -0,0 +1,13 @@
+package com.billsystem.parctice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcticeApplication.class, args);
+	}
+
+}
---
+++ parctice/src/test/java/com/billsystem/parctice/ParcticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billsystem.parctice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
